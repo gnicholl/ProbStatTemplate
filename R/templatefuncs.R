b2e0e46d6448f281bc1f5fc1e1991fb4af8d9e06
gnich <gradon.nicholls@gmail.com>
2023-06-06 14:30:31 -0400

    add beamer template

--- R/templatefuncs.R
+++ R/templatefuncs.R
@@ -5,3 +5,9 @@ probstattemplate <- function(...){
   rmarkdown::pdf_document(template = templ,
                           ...)
 }
+
+probstatbeamer <- function(...){
+  templ <- system.file("rmarkdown", "templates", "probstatbeamer", "resources", "template.tex", package = "ProbStatTemplate")
+  rmarkdown::beamer_presentation(template = templ,
+                          ...)
+}
